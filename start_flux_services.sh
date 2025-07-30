9904402e66995f9b31746e0908e02acdf54084e5
Kevin Smith <vndinmchine@gmail.com>
2025-07-30 17:13:39 -0400

    Update start_flux_services.sh

--- start_flux_services.sh
+++ start_flux_services.sh
@@ -3,7 +3,7 @@ source /home/smithkt/flux_schnell_cpu/flux_env/bin/activate
 cd /home/smithkt/flux_api
 
 # Start FastAPI server in background
-/home/smithkt/flux_schnell_cpu/flux_env/bin/uvicorn flux_api:app --host 0.0.0.0 --port 8000 &
+/home/smithkt/flux_schnell_cpu/flux_env/bin/uvicorn flux_api:app --host 0.0.0.0 --port 8000  --limit-max-request-size 100 &
 
 # Start worker pool (waits)
 python start_workers.py
